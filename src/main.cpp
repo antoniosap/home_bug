c7486e926db1efabcade5923459702c63f138c6d
Antonio Sapuppo <antoniosapuppo@yahoo.it>
2021-07-25 15:54:06 +0200

    libraries and first coding

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,134 @@
-#include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+//-------------------------------------------------------------------------------------------------------
+/*
+  Project Name: TM1638plus (arduino library)
+  File: TM1638plus_HELLOWORLD_Model2.ino
+  Description: A Basic test file for model 2, TM1638 module(16 KEY 16 pushbuttons).
+  Author: Gavin Lyons.
+  Created Feb 2020
+  URL: https://github.com/gavinlyonsrepo/TM1638plus
+*/
+
+#include <Arduino.h>
+#include <TM1638plus_Model2.h>
+
+// GPIO I/O pins on the Arduino connected to strobe, clock, data, pick on any I/O pin you want.
+#define  STROBE_TM 4  // strobe = GPIO connected to strobe line of module
+#define  CLOCK_TM 6  // clock = GPIO connected to clock line of module
+#define  DIO_TM 7 // data = GPIO connected to data line of module
+bool swap_nibbles = false; //Default is false if left out, see issues section in readme at URL
+bool high_freq = false; //default false, If using a high freq CPU > ~100 MHZ set to true. 
+
+// Constructor object Init the module
+TM1638plus_Model2 tm(STROBE_TM, CLOCK_TM , DIO_TM, swap_nibbles, high_freq);
+
+//-------------------------------------------------------------------------------------------------------
+/*********************************************************
+This is a library for the MPR121 12-channel Capacitive touch sensor
+
+Designed specifically to work with the MPR121 Breakout in the Adafruit shop 
+  ----> https://www.adafruit.com/products/
+
+These sensors use I2C communicate, at least 2 pins are required 
+to interface
+
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+
+Written by Limor Fried/Ladyada for Adafruit Industries.  
+BSD license, all text above must be included in any redistribution
+**********************************************************/
+
+#include <Wire.h>
+#include "Adafruit_MPR121.h"
+
+#ifndef _BV
+#define _BV(bit) (1 << (bit)) 
+#endif
+
+// You can have up to 4 on one i2c bus but one is enough for testing!
+Adafruit_MPR121 cap = Adafruit_MPR121();
+
+// Keeps track of the last pins touched
+// so we know when buttons are 'released'
+uint16_t lasttouched = 0;
+uint16_t currtouched = 0;
+
+//-------------------------------------------------------------------------------------------------------
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include "WiFiManager.h"          //https://github.com/tzapu/WiFiManager
+
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  //if you used auto generated SSID, print it
+  Serial.println(myWiFiManager->getConfigPortalSSID());
 }
 
+//-------------------------------------------------------------------------------------------------------
+void setup() {
+  Serial.begin(115200);
+
+  while (!Serial) { // needed to keep leonardo/micro from starting too fast!
+    delay(10);
+  }
+  
+  Serial.println("Adafruit MPR121 Capacitive Touch sensor test"); 
+  
+  // Default address is 0x5A, if tied to 3.3V its 0x5B
+  // If tied to SDA its 0x5C and if SCL then 0x5D
+  if (!cap.begin(0x5A)) {
+    Serial.println("MPR121 not found, check wiring?");
+    while (1);
+  }
+  Serial.println("MPR121 found!");
+
+ tm.displayBegin();
+}
+
+//-------------------------------------------------------------------------------------------------------
 void loop() {
-  // put your main code here, to run repeatedly:
+	tm.DisplayStr("helowrld", 0);  // Display "helowrld" in seven segments with zero decimal points set.
+	delay(5000);
+
+  // Get the currently touched pads
+  currtouched = cap.touched();
+  
+  for (uint8_t i=0; i<12; i++) {
+    // it if *is* touched and *wasnt* touched before, alert!
+    if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
+      Serial.print(i); Serial.println(" touched");
+    }
+    // if it *was* touched and now *isnt*, alert!
+    if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
+      Serial.print(i); Serial.println(" released");
+    }
+  }
+
+  // reset our state
+  lasttouched = currtouched;
+
+  // comment out this line for detailed data from the sensor!
+  return;
+  
+  // debugging info, what
+  Serial.print("\t\t\t\t\t\t\t\t\t\t\t\t\t 0x"); Serial.println(cap.touched(), HEX);
+  Serial.print("Filt: ");
+  for (uint8_t i=0; i<12; i++) {
+    Serial.print(cap.filteredData(i)); Serial.print("\t");
+  }
+  Serial.println();
+  Serial.print("Base: ");
+  for (uint8_t i=0; i<12; i++) {
+    Serial.print(cap.baselineData(i)); Serial.print("\t");
+  }
+  Serial.println();
+  
+  // put a delay so it isn't overwhelming
+  delay(100);
 }
